7f40bdc13950fb6ec36f1a110d763601a83c20a7
someone4 <tamhoaanhlinh1995@gmail.com>
2022-04-25 22:09:13 +0700

    them lenh thong bao loi

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 // if(!$conn) {
 	// 	echo 'Connection error: ' .mysqli_connect_error();
 	// }
+	error_reporting(E_ALL);
+	ini_set("display_errors", 1);
 	
 	include('config/db_connect.php');
 // write query for all pizzas: Lựa chọn 1 hoặc nhiều hoặc tất cả các chỉ mục để lấy ra từ dtb, 
