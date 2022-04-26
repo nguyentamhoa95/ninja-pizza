3d7dc8c66a424fa02bf3fa1d32e51bae63ce824b
someone <tamhoaanhlinh1995@gmail.com>
2022-04-26 09:23:03 +0700

    fixxxxxxxxx

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 // connect database : kết nối với dtb
-$conn = mysqli_connect('127.0.0.1', 'shaun', 'text123', 'ninja_pizza');
+$conn = mysqli_connect('localhost', 'shaun', 'text123', 'ninja_pizza');
 	
 // check connection: kiểm tra kết nối 
 	if(!$conn) {
